89f07ff83070270ba80b25fbf34debd26bf959e8
Yu Sun <936605403@qq.com>
2022-02-11 10:56:32 +0800

    Update base.py

--- romp/base.py
+++ romp/base.py
@@ -33,6 +33,7 @@ class Base(object):
         self._init_log(hparams_dict)
         self._init_params()
         if self.save_visualization_on_img:
+            print('Loading {} renderer as visualizer'.format(args().renderer))
             self.visualizer = Visualizer(resolution=(512,512), result_img_dir=self.result_img_dir, renderer_type=args().renderer)
 
     def _build_model_(self):

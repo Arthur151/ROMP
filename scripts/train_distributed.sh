51c3f2d05e367e984bee9d60d632e6a8ea16f7f8
Yu Sun <936605403@qq.com>
2022-05-07 09:26:23 +0800

    Update train_distributed.sh

--- scripts/train_distributed.sh
+++ scripts/train_distributed.sh
@@ -8,5 +8,5 @@ TRAIN_CONFIGS='configs/v7.yml'
 GPUS=$(cat $TRAIN_CONFIGS | shyaml get-value ARGS.GPUS)
 DATASET=$(cat $TRAIN_CONFIGS | shyaml get-value ARGS.dataset)
 TAB=$(cat $TRAIN_CONFIGS | shyaml get-value ARGS.tab)
-#CUDA_VISIBLE_DEVICES=${GPUS} python -u -m torch.distributed.launch --nproc_per_node=4  romp.train --GPUS=${GPUS} --configs_yml=${TRAIN_CONFIGS}
-CUDA_VISIBLE_DEVICES=${GPUS} nohup python -u -m torch.distributed.launch --nproc_per_node=4 romp.train --GPUS=${GPUS} --configs_yml=${TRAIN_CONFIGS} --distributed_training=1 > '../log/'${TAB}'_'${DATASET}'_g'${GPUS}.log 2>&1 &
+#CUDA_VISIBLE_DEVICES=${GPUS} torchrun --nproc_per_node=4 -m romp.train --GPUS=${GPUS} --configs_yml=${TRAIN_CONFIGS} --distributed_training=1
+CUDA_VISIBLE_DEVICES=${GPUS} nohup torchrun --nproc_per_node=4 -m romp.train --GPUS=${GPUS} --configs_yml=${TRAIN_CONFIGS} --distributed_training=1 > '../log/'${TAB}'_'${DATASET}'_g'${GPUS}.log 2>&1 &

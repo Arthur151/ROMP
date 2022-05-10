42f40fdc7be6a282657dccff7623c5872ec947ee
Yu Sun <yusun@stu.hit.edu.cn>
2022-05-10 20:51:21 +0800

    fix

--- simple_romp/upload_pypi.sh
+++ simple_romp/upload_pypi.sh
@@ -1,2 +1,2 @@
-twine upload --repository-url https://test.pypi.org/legacy/ dist/simple_romp-1*
-#twine upload dist/*
+#twine upload --repository-url https://test.pypi.org/legacy/ dist/simple_romp-1*
+twine upload dist/*
